2db3447e9e6686aa1c4c60c127bdbf56c16c1ddf
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-03-29 23:23:52 -0300

    Updated docs.

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     license = 'Apache License 2.0',
     keywords = 'correios brasil api',
     url = 'http://github.com/guilhermechapiewski/correios-api-py/',
-    long_description = 'API Python para obter informacoes de encomendas/Sedex dos Correios. Para mais detalhes veja a documentação no Github: http://github.com/guilhermechapiewski/correios-api-py/blob/master/README.textile',
+    long_description = 'API Python para obter informacoes de encomendas/Sedex dos Correios. Para mais detalhes veja a documentacao no Github: http://github.com/guilhermechapiewski/correios-api-py/blob/master/README.textile',
     install_requires = ['BeautifulSoup >= 3.1.0'],
 )
