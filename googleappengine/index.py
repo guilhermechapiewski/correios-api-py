0bf2f64b510bbc50fdb0bdb1687a51c50df89a53
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-03-30 00:42:00 -0300

    Backend for YQL ready.

--- googleappengine/index.py
+++ googleappengine/index.py
@@ -5,21 +5,30 @@ from correios import Correios
 
 class YQLPage(webapp.RequestHandler):
     def get(self):
-        encomenda = Correios.encomenda('ES446391025BR')
-        
         self.response.headers['Content-Type'] = 'text/xml'
         self.response.out.write('<?xml version="1.0" encoding="UTF-8"?>')
-        self.response.out.write('<result>')
         
-        for status in encomenda.status:
-            self.response.out.write('   <status>')
-            self.response.out.write('       <data><![CDATA[%s]]></data>' % status.data)
-            self.response.out.write('       <local><![CDATA[%s]]></local>' % status.local)
-            self.response.out.write('       <situacao><![CDATA[%s]]></situacao>' % status.situacao)
-            self.response.out.write('       <detalhes><![CDATA[%s]]></detalhes>' % status.detalhes)
-            self.response.out.write('   </status>')
+        encomenda = None
+        try:
+            encomenda = Correios.encomenda(self.request.get('numero', default_value=''))
+        except Exception, e:
+            pass #will return empty result :)
         
-        self.response.out.write('</result>')
+        if encomenda:
+            self.response.out.write('<result>')
+        
+            for status in encomenda.status:
+                detalhes = status.detalhes if status.detalhes else ''
+                self.response.out.write('   <status>')
+                self.response.out.write('       <data><![CDATA[%s]]></data>' % status.data)
+                self.response.out.write('       <local><![CDATA[%s]]></local>' % status.local)
+                self.response.out.write('       <situacao><![CDATA[%s]]></situacao>' % status.situacao)
+                self.response.out.write('       <detalhes><![CDATA[%s]]></detalhes>' % detalhes)
+                self.response.out.write('   </status>')
+        
+            self.response.out.write('</result>')
+        else:
+            self.response.out.write('<result />')
 
 application = webapp.WSGIApplication(
                                      [('/yql.*', YQLPage)],
