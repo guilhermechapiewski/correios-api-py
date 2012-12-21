a863cce718a1ff14c91ee5742ca1fb61b25de26e
Alê Borba <alexandre.borba@imasters.com.br>
2012-12-21 10:56:58 -0200

    Packages adjusted

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name = 'packtrack',
     version = '0.1.0',
-    packages = ['packtrack'],
+    packages = ['correios', 'dhl_gm'],
     author = 'Ale Borba',
     author_email = 'ale.borba@codingforchange.com',
     description = 'API Python para obter informacoes de encomendas',
