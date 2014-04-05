754b7e2f9b573ba4f51ee908932b0b7bbfd675c6
Ale Borba <ale.alvesborba@gmail.com>
2014-04-05 09:20:06 -0300

    Nova versão

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 setup(
     name = 'packtrack',
-    version = '0.1.0',
-    packages = ['correios', 'dhl_gm'],
+    version = '0.2.0',
+    packages = ['correios', 'dhl_gm', 'royal'],
     author = 'Ale Borba',
     author_email = 'ale.borba@codingforchange.com',
     description = 'API Python para obter informacoes de encomendas',
@@ -12,5 +12,5 @@ setup(
     keywords = 'encomendas track api',
     url = 'https://github.com/aleborba/packtrack',
     long_description = 'API Python para obter informacoes de encomendas. Para mais detalhes veja a documentacao no Github: https://github.com/aleborba/packtrack/blob/master/README.textile',
-    install_requires = ['BeautifulSoup >= 3.1.0', 'requests >= 0.14.2'],
+    install_requires = ['BeautifulSoup >= 3.1.0', 'requests >= 0.14.2', 'beautifulsoup4 >= 4.3.2'],
 )
