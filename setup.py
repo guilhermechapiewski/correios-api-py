496896a8f905e87576402b6b17a99c1b91a0aebc
Ale Borba <ale.alvesborba@gmail.com>
2014-04-05 10:33:48 -0300

    Mudando a versão e adequando o README

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = 'packtrack',
-    version = '0.2.0',
+    version = '1.0.0',
     packages = ['packtrack'],
     author = 'Ale Borba',
     author_email = 'ale.borba@codingforchange.com',
