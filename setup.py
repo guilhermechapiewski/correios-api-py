a37e12a2f66a5c7db3377488df6a9ed0d986a625
Ale Borba <ale.alvesborba@gmail.com>
2014-04-09 19:08:39 -0300

    Ajustando versão

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = 'packtrack',
-    version = '1.0.0',
+    version = '1.0.1',
     packages = ['packtrack'],
     author = 'Ale Borba',
     author_email = 'ale.borba@codingforchange.com',
