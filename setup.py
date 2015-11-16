80e5d0709a8a17cd5d8e68be1b535a4b6f4788f4
Iuri de Silvio <iurisilvio@gmail.com>
2015-11-16 20:03:46 -0200

    Version bump: 1.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = 'packtrack',
-    version = '1.0.3',
+    version = '1.0.4',
     packages = ['packtrack'],
     author = 'Ale Borba',
     author_email = 'ale.borba@codingforchange.com',
