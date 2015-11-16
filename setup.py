2e074d8cf4c1228ea12cb0aec3fe88afa0aae4a5
Iuri de Silvio <iurisilvio@gmail.com>
2015-11-16 20:10:54 -0200

    Another version bump: 1.0.5
    PyPI already has a 1.0.4. I don't know why it is not commited. :/

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = 'packtrack',
-    version = '1.0.4',
+    version = '1.0.5',
     packages = ['packtrack'],
     author = 'Ale Borba',
     author_email = 'ale.borba@codingforchange.com',
