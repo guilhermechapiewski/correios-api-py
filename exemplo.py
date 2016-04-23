ef766e9e09abe649ad1284d5c5f0d3ea4decaf42
Madson Dias <madson.dias@ppgcc.ifce.edu.br>
2016-04-23 11:02:04 -0300

    update example

--- exemplo.py
+++ exemplo.py
@@ -1,11 +1,12 @@
 from correios import Correios
 
-encomenda = Correios.encomenda("ES446391025BR")
-print encomenda.numero
-
-for status in encomenda.status:
-    print "Data: %s" % status.data
-    print "Local: %s" % status.local
-    print "Situacao: %s" % status.situacao
-    print "Detalhes: %s" % status.detalhes
-    print
+try:
+  encomenda = Correios.encomenda("PJ514632791BR")
+  for status in encomenda.status:
+      print "Data: %s" % status.data
+      print "Local: %s" % status.local
+      print "Situacao: %s" % status.situacao
+      print "Detalhes: %s" % status.detalhes
+      print
+except Exception as e:
+  print e
