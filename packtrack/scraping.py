3a011f501fb3025222f58d7fab2cc299a059ccfc
Iuri de Silvio <iurisilvio@gmail.com>
2016-06-12 12:40:23 -0300

    Implementa novo site dos Correios.

--- packtrack/scraping.py
+++ packtrack/scraping.py
@@ -1,4 +1,5 @@
 import re
+import urllib
 import urllib2
 
 from BeautifulSoup import BeautifulSoup
@@ -6,16 +7,15 @@ from BeautifulSoup import BeautifulSoup
 from correios import Encomenda, Status
 
 
-class CorreiosWebsiteScraper(object):
+class _CorreiosWebsiteScraperBase(object):
 
     def __init__(self, http_client=urllib2):
-        self.url = 'http://websro.correios.com.br/sro_bin/txect01$.QueryList?P_ITEMCODE=&P_LINGUA=001&P_TESTE=&P_TIPO=001&P_COD_UNI='
         self.http_client = http_client
 
     def get_encomenda_info(self, numero):
-        url = '%s%s' % (self.url, urllib2.quote(numero))
+        req = self._req(numero)
         try:
-            request = self.http_client.urlopen(url)
+            request = self.http_client.urlopen(req)
         except urllib2.HTTPError:
             return None
 
@@ -30,6 +30,67 @@ class CorreiosWebsiteScraper(object):
             for status in self._get_all_status_from_html(html):
                 encomenda.adicionar_status(status)
             return encomenda
+
+
+class CorreiosWebsiteScraper(_CorreiosWebsiteScraperBase):
+    url = 'http://www2.correios.com.br/sistemas/rastreamento/resultado.cfm'
+
+    def _req(self, numero):
+        data = {
+            'P_LINGUA': '001',
+            'P_TIPO': '001',
+            'objetos': numero,
+        }
+        headers = {
+            'Referer': self.url,  # page refuses the call without this referer
+        }
+        req = self.http_client.Request(self.url,
+                                       data=urllib.urlencode(data),
+                                       headers=headers)
+        return req
+
+    def _text(self, value):
+        value = BeautifulSoup(value.strip()).text
+        return value.replace('&nbsp;', ' ')
+
+    def _get_all_status_from_html(self, html):
+        html_info = re.search('.*(<table.*</table>).*', html, re.S)
+        status = []
+        if not html_info:
+            return status
+
+        table = html_info.group(1)
+        soup = BeautifulSoup(table)
+
+        for tr in soup.table:
+            try:
+                tds = tr.findAll('td')
+            except AttributeError:
+                continue
+            for td in tds:
+                content = td.renderContents().replace('\r', ' ') \
+                    .split('<br />')
+                class_ = td['class']
+                if class_ == 'sroDtEvent':
+                    data = '%s %s' % (content[0].strip(), content[1].strip())
+                    local = '/'.join(self._text(content[2]).rsplit(' / ', 1)).upper()
+                elif class_ == 'sroLbEvent':
+                    situacao = self._text(content[0])
+                    detalhes = self._text(content[1])
+                    if detalhes:
+                        detalhes = u'%s %s' % (situacao, detalhes)
+                    status.append(Status(data=data, local=local,
+                                         situacao=situacao, detalhes=detalhes))
+        return status
+
+
+class CorreiosWebsroScraper(_CorreiosWebsiteScraperBase):
+    url = 'http://websro.correios.com.br/sro_bin/txect01$.QueryList?P_ITEMCODE=&P_LINGUA=001&P_TESTE=&P_TIPO=001&P_COD_UNI='
+
+    def _req(self, numero):
+        url = '%s%s' % (self.url, numero)
+        req = self.http_client.Request(url)
+        return req
 
     def _get_all_status_from_html(self, html):
         html_info = re.search('.*(<table.*</TABLE>).*', html, re.S)
--- tests/scraping_test.py
+++ tests/scraping_test.py
@@ -2,67 +2,105 @@
 import os
 import unittest
 
-from mockito import *
+from mockito import when, Mock, any as ANY
 
-from packtrack.scraping import CorreiosWebsiteScraper
+from packtrack.scraping import CorreiosWebsiteScraper, CorreiosWebsroScraper
 from packtrack.dhl_gm import DhlGmTracker
 
+
 class CorreiosWebsiteScraperTest(unittest.TestCase):
-    
+
+    def _assert_status(self, status, data, local, situacao, detalhes):
+        self.assertEqual(data, status.data)
+        self.assertEqual(local, status.local)
+        self.assertEqual(situacao, status.situacao)
+        self.assertEqual(detalhes, status.detalhes)
+
     def test_should_get_data_from_correios_website(self):
         example_file = open('%s/tests/correios_website/exemplo_rastreamento_correios1.html' % os.getcwd())
         sample_html = example_file.read()
         example_file.close()
-        
+
         urllib2_mock = Mock()
         request_mock = Mock()
-        when(urllib2_mock).urlopen('http://websro.correios.com.br/sro_bin/txect01$.QueryList?P_ITEMCODE=&P_LINGUA=001&P_TESTE=&P_TIPO=001&P_COD_UNI=ES446391025BR').thenReturn(request_mock)
+        when(urllib2_mock).urlopen(ANY()).thenReturn(request_mock)
         when(request_mock).read().thenReturn(sample_html)
-        
+
         correios_website_scraper = CorreiosWebsiteScraper(urllib2_mock)
+        numero = 'PJ859656941BR'
+        encomenda = correios_website_scraper.get_encomenda_info(numero)
+        self.assertEqual(numero, encomenda.numero)
+
+        self._assert_status(encomenda.status[0], u'07/06/2016 13:17',
+                            u'SANTO ANDRE/SP', u'Objeto postado', u'')
+
+        detalhes = u'Objeto encaminhado de Agência dos Correios em Santo Andre / SP para Unidade Operacional em Sao Paulo / SP'
+        self._assert_status(encomenda.status[1], u'07/06/2016 13:55',
+                            u'SANTO ANDRE/SP', u'Objeto encaminhado', detalhes)
+
+        detalhes = u'Objeto encaminhado de Unidade Operacional em Sao Paulo / SP para Unidade de Distribuição em SAO JOSE DOS CAMPOS / SP'
+        self._assert_status(encomenda.status[2], u'08/06/2016 15:51',
+                            u'SAO PAULO/SP', u'Objeto encaminhado', detalhes)
+
+
+class CorreiosWebsroScraperTest(unittest.TestCase):
+
+    def test_should_get_data_from_correios_website(self):
+        example_file = open('%s/tests/correios_website/websro/exemplo_rastreamento_correios1.html' % os.getcwd())
+        sample_html = example_file.read()
+        example_file.close()
+
+        urllib2_mock = Mock()
+        request_mock = Mock()
+        when(urllib2_mock).urlopen(ANY()).thenReturn(request_mock)
+        when(request_mock).read().thenReturn(sample_html)
+
+        correios_website_scraper = CorreiosWebsroScraper(urllib2_mock)
         encomenda = correios_website_scraper.get_encomenda_info('ES446391025BR')
         assert encomenda.numero == 'ES446391025BR'
-        
+
         assert encomenda.status[0].data == u'27/01/2009 16:35'
         assert encomenda.status[0].local == u'ACF FENIX - ITAPECERICA DA SERRA/SP'
         assert encomenda.status[0].situacao == u'Postado'
         assert encomenda.status[0].detalhes == None
-        
+
         assert encomenda.status[1].data == u'27/01/2009 18:51'
         assert encomenda.status[1].local == u'ACF FENIX - ITAPECERICA DA SERRA/SP'
         assert encomenda.status[1].situacao == u'Encaminhado'
         assert encomenda.status[1].detalhes == u'Em tr\xc3\xa2nsito para CTE JAGUARE - SAO PAULO/SP'
-        
+
         assert encomenda.status[2].data == u'28/01/2009 03:36'
         assert encomenda.status[2].local == u'CTE JAGUARE - SAO PAULO/SP'
         assert encomenda.status[2].situacao == u'Encaminhado'
         assert encomenda.status[2].detalhes == u'Encaminhado para CTE SAUDE - SAO PAULO/SP'
-        
+
         assert encomenda.status[3].data == u'28/01/2009 06:40'
         assert encomenda.status[3].local == u'CTE SAUDE - SAO PAULO/SP'
         assert encomenda.status[3].situacao == u'Encaminhado'
         assert encomenda.status[3].detalhes == u'Encaminhado para CEE MOEMA - SAO PAULO/SP'
-        
+
         assert encomenda.status[4].data == u'28/01/2009 14:17'
         assert encomenda.status[4].local == u'CEE MOEMA - SAO PAULO/SP'
         assert encomenda.status[4].situacao == u'Saiu para entrega'
-        assert encomenda.status[4].detalhes == None
-        
+        assert encomenda.status[4].detalhes is None
+
         assert encomenda.status[5].data == u'28/01/2009 17:49'
         assert encomenda.status[5].local == u'CEE MOEMA - SAO PAULO/SP'
         assert encomenda.status[5].situacao == u'Entregue'
-        assert encomenda.status[5].detalhes == None
+        assert encomenda.status[5].detalhes is None
+
 
 class DhlGmBaseTest(object):
 
     expected = {
-            'GM555113775306714774': [{
-                                'atividade': 'ARRIVAL ORIGIN DHL GLOBAL MAIL FACILITY',
-                                'data': '10/11/2012',
-                                'hora': '01:18 AM CT',
-                                'localizacao': 'Des Plaines, IL'
-            }]
+        'GM555113775306714774': [{
+            'atividade': 'ARRIVAL ORIGIN DHL GLOBAL MAIL FACILITY',
+            'data': '10/11/2012',
+            'hora': '01:18 AM CT',
+            'localizacao': 'Des Plaines, IL'
+        }]
     }
+
 
 class DhlGmTrackerTest(unittest.TestCase, DhlGmBaseTest):
 
